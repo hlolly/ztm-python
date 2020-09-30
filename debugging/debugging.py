b21fd0b4eed18e9c61cf177229e6ad877a4106f7
Patrícia Faria <63451914+hlolly@users.noreply.github.com>
2020-09-30 15:21:58 +0200

    Add files via upload

---
+++ debugging/debugging.py
@@ -0,0 +1,17 @@
+# Linting
+# IDE / EDITOR
+# READ ERRORS
+# PDB
+import pdb
+
+def add(num1, num2):
+    pdb.set_trace()
+    t = 4*5
+    return num1 + num2
+
+add(4, 'H')
+
+# help in terminal
+# step / continue / a / w /
+# exit
+# remove pdb
